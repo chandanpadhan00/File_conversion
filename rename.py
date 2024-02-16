ef9f01611099db3d201f5bc78fc4c9649f5d8ed5
chandanpadhan00 <142711074+chandanpadhan00@users.noreply.github.com>
2024-02-16 19:57:21 +0530

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,12 +1,9 @@
 import os
 
-folder_path = "/path/to/your/folder"  # Replace with the actual path to your folder
+folder_path = "path/to/your/folder"  # Replace with the actual path to your folder
 
 for filename in os.listdir(folder_path):
-    if filename.endswith(".csv") and filename.lower().endswith(".xlsx.csv"):
-        # Construct the new filename by removing the ".xlsx" part
-        new_filename = os.path.join(folder_path, filename[:-5])
-        
-        # Rename the file
-        os.rename(os.path.join(folder_path, filename), new_filename)
-        print(f"Renamed: {filename} to {new_filename}")
+    if filename.endswith(".xlsx"):
+        full_path = os.path.join(folder_path, filename)
+        new_name = os.path.join(folder_path, os.path.splitext(filename)[0])
+        os.rename(full_path, new_name)
