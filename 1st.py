0f3770dbb25c6cc0450528286f9d72c0c63fb482
chandanpadhan00 <142711074+chandanpadhan00@users.noreply.github.com>
2024-04-05 13:07:19 +0530

    Update 1st.py

--- 1st.py
+++ 1st.py
@@ -13,16 +13,11 @@ session = boto3.Session(
     aws_secret_access_key=aws_secret_access_key
 )
 
-# Create a SparkSession with the AWS credentials
-spark = (
-    SparkSession.builder
-    .appName("Export Tables to S3")
-    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider")
-    .getOrCreate()
-)
-spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider")
-spark.sparkContext._jsc.hadoopConfiguration().set("com.amazonaws.services.s3a.enableV4", "true")
-spark.sparkContext._awsCredentialsProvider = session.get_credentials()
+# Create a SparkSession
+spark = SparkSession.builder.appName("Export Tables to S3").getOrCreate()
+
+# Set the AWS credentials in the Hadoop configuration
+spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", aws_access_key_id)
+spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", aws_secret_access_key)
 
 # ... (the rest of your code remains the same)
